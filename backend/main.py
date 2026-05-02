60deb5ce9d54335919a6e0b2be33c7e828d3611c
Anubhav yadav <ay8400673@gmail.com>
2026-05-03 03:19:14 +0530

    Implement login endpoint with JWT token generation and password verification

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,17 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 import models, schemas
 from database import engine, SessionLocal
+import jwt
+from datetime import datetime, timedelta, timezone
 
 # Database table create karna
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+# Ye hamara secret key hai (jaise ek master password). Ise kisi ko nahi batana chahiye!
+SECRET_KEY = "my_super_secret_key" 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Token 30 minute me expire ho jayega
 
 # Password Hash karne ka setup
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -48,3 +54,34 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     
     return new_user
+
+# --- LOGIN API ---
+@app.post("/login/")
+def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+    # 1. Database me check karo ki is email se koi user hai ya nahi
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    
+    # Agar user nahi mila, toh error do (403 Forbidden)
+    if not user:
+        raise HTTPException(status_code=403, detail="Invalid Credentials")
+    
+    # 2. Agar email mil gaya, toh password match karo
+    is_password_correct = pwd_context.verify(user_credentials.password, user.hashed_password)
+    
+    if not is_password_correct:
+        raise HTTPException(status_code=403, detail="Invalid Credentials")
+    
+    # 3. Email aur Password dono sahi hain! Ab "Wristband" (Token) banao
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    # Payload: Ye wo data hai jo token ke andar chhipa hoga
+    token_data = {
+        "sub": user.email, # sub ka matlab subject (user ki pehchan)
+        "role": user.role, # Yahan pata chalega ki ye 'admin' hai ya 'user'
+        "exp": expire      # Expiry time
+    }
+    
+    # Token Generate karna
+    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+    
+    return {"access_token": token, "token_type": "bearer"}
