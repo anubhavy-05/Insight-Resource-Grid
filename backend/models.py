796d7810acc160176012c4dbb19b500611b1e905
Anubhav yadav <ay8400673@gmail.com>
2026-05-16 10:20:18 +0530

    Refactor models.py to consolidate imports and enhance Resource class definition

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
@@ -13,3 +18,20 @@ class User(Base):
     role = Column(String, default="user") # Default sab "user" honge
     
     is_active = Column(Boolean, default=True)
+    
+
+    # --- RESOURCE TABLE ---
+class Resource(Base):
+    __tablename__ = "resources"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    link = Column(String) 
+    status = Column(String, default="Pending") # Naya data hamesha pending rahega
+    
+    # Ye Foreign Key User table ki ID se connect karegi
+    owner_id = Column(Integer, ForeignKey("users.id")) 
+    
+    # Ye relationship hume backend me data fetch karne me madad karegi
+    owner = relationship("User")
